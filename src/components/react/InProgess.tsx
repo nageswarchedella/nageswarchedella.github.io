05ef389840cf14df7b5bce32a4e9208104695a14
Nageswar Chedella <nageswarchedella@gmail.com>
2024-04-14 01:36:04 +0530

    Initial commit

---
+++ src/components/react/InProgess.tsx
@@ -0,0 +1,7 @@
+const InProgess = () => {
+  return (
+    <div>This website is under construction!</div>
+  )
+}
+
+export default InProgess;
---
+++ src/components/react/Resume.tsx
@@ -0,0 +1,13 @@
+const Resume = () => {
+  return (
+    <>
+      <div>
+        Thanks for coming here! ;-)
+        <br />
+        I am working on this webpage
+      </div>
+    </>
+  );
+};
+
+export default Resume;
